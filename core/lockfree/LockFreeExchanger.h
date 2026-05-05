ae11b9541df13dde71a60ee7204b8e5825707c6f
FengxunSui <13661918512@126.com>
2026-05-05 15:58:44 +0800

    add lockfreeechanger

---
+++ core/lockfree/LockFreeExchanger.h
@@ -0,0 +1,68 @@
+#include <atomic>
+#include <chrono>
+#include <optional>
+#include <system_error>
+using namespace std::chrono;
+
+struct LockFreeExchanger {
+public:
+  enum class State : int { EMPTY, WAITING, BUSY };
+  using enum State;
+
+  std::optional<void *> exchange(void *myItem, milliseconds timeout) {
+    auto deadline = steady_clock::now() + timeout;
+    State curr_state;
+    StatePtr rt_ptr = slot.load();
+    StatePtr my_ptr;
+    while (true) {
+      if (steady_clock::now() > deadline)
+        throw std::runtime_error("timed out");
+      curr_state = rt_ptr.state();
+      switch (curr_state) {
+      case EMPTY:
+        my_ptr = StatePtr(myItem, WAITING);
+        if (slot.compare_exchange_strong(rt_ptr, my_ptr)) {
+          while (steady_clock::now() < deadline) {
+            rt_ptr = slot.load();
+            if (rt_ptr.state() == BUSY) {
+              slot.store(StatePtr());
+              return rt_ptr.ptr();
+            }
+          }
+          if (slot.compare_exchange_strong(my_ptr, StatePtr())) {
+            throw std::runtime_error("timed out");
+          } else {
+            slot.store(StatePtr());
+            return my_ptr.ptr();
+          }
+        }
+        break;
+      case WAITING:
+        my_ptr = StatePtr(myItem, BUSY);
+        if (slot.compare_exchange_strong(rt_ptr, my_ptr))
+          return rt_ptr.ptr();
+        break;
+      case BUSY:
+        break;
+      default:
+        break;
+      }
+    }
+  };
+
+private:
+  struct StatePtr {
+    uintptr_t data;
+
+    static constexpr uintptr_t STATE_MASK = 0x3; // 2 bits for state
+    static constexpr uintptr_t PTR_MASK = ~STATE_MASK;
+
+    StatePtr(void *p = nullptr, State s = EMPTY) {
+      data = (reinterpret_cast<uintptr_t>(p) & PTR_MASK) |
+             static_cast<uintptr_t>(s);
+    }
+    void *ptr() const { return reinterpret_cast<void *>(data & PTR_MASK); }
+    State state() const { return static_cast<State>(data & STATE_MASK); }
+  };
+  std::atomic<StatePtr> slot;
+};
